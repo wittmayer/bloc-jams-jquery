3058c3a41f0b78d7b2d80677771756130919a95b
Melissa <melissa.wittmayer@gmail.com>
2019-04-17 09:11:10 -0700

    checkpoint correction - add album art

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,5 +1,5 @@
 {
-  $('img#album-cover-art').attr('src', album.albumArtURl);
+  $('img#album-cover-art').attr('src', album.albumArtUrl);
   $('#album-title').text(album.title);
   $('.artist').text(album.artist);
   $('#release-info').text(album.releaseInfo);
